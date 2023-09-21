5e04537a24e211102886af77744685ced1f06d0a
Zaim211 <yousseftitzaim@gmail.com>
2023-09-21 15:19:45 -0700

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,8 +21,8 @@ void bubble_sort(int *array, size_t size)
 			int tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				swap = 1;
 				print_array(array, size);
+				swap = 1;
 			}
 		}
 		if (swap == 0)
